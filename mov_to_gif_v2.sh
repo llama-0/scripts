79ee7a9c8843a859019b5b96ed1352b0278389f5
llama-0 <anrozan93@mail.ru>
2021-05-17 10:29:05 +0300

    mov_to_gif converter simplyfied for everyday use

---
+++ mov_to_gif_v2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Notice: run 'brew install ffmpeg; brew install gifsicle' if needed
+
+if [[ $# -eq 0 ]]
+    then
+    echo 'usage: ./mov_to_gif input.mov'
+    echo 'This will generate a file input.gif'
+    exit
+fi
+
+IN=$1
+TMP=${IN%.*}
+OUT=$TMP.gif
+ffmpeg -i $IN -pix_fmt rgb8 -r 10 $OUT && gifsicle -O3 $OUT -o $OUT
